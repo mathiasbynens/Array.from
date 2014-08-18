f40dba4de5979f2752675378f7b45d7474c70540
Mathias Bynens <mathias@qiwi.be>
2014-08-18 10:00:14 +0200

    Revert coding style changes

--- array-from.js
+++ array-from.js
@@ -1,9 +1,9 @@
 /*! http://mths.be/array-from v0.1.0 by @mathias */
 if (!Array.from) {
-	(function () {
+	(function() {
 		'use strict';
-		var defineProperty = (function () {
-			// IE 8 only supports `Object.defineProperty` on DOM elements
+		var defineProperty = (function() {
+			// IE 8 only supports `Object.defineProperty` on DOM elements.
 			try {
 				var object = {};
 				var $defineProperty = Object.defineProperty;
@@ -14,24 +14,31 @@ if (!Array.from) {
 			};
 		}());
 		var toStr = Object.prototype.toString;
-		var isCallable = function (fn) {
-			return typeof fn === 'function' || toStr.call(fn) === '[object Function]';
+		var isCallable = function(fn) {
+			// In a perfect world, the `typeof` check would be sufficient. However,
+			// in Chrome 1–12, `typeof /x/ == 'object'`, and in IE 6–8
+			// `typeof alert == 'object'` and similar for other host objects.
+			return typeof fn == 'function' || toStr.call(fn) == '[object Function]';
 		};
-		var toInteger = function (value) {
+		var toInteger = function(value) {
 			var number = Number(value);
-			if (isNaN(number)) { return 0; }
-			if (number === 0 || !isFinite(number)) { return number; }
+			if (isNaN(number)) {
+				return 0;
+			}
+			if (number == 0 || !isFinite(number)) {
+				return number;
+			}
 			return (number > 0 ? 1 : -1) * Math.floor(Math.abs(number));
 		};
 		var maxSafeInteger = Math.pow(2, 53) - 1;
-		var toLength = function (value) {
+		var toLength = function(value) {
 			var len = toInteger(value);
 			return Math.min(Math.max(len, 0), maxSafeInteger);
 		};
-		var from = function (arrayLike) {
+		var from = function(arrayLike) {
 			var C = this;
 			if (arrayLike == null) {
-				throw TypeError("Array.from requires an array-like object - not null or undefined");
+				throw new TypeError('`Array.from` requires an array-like object, not `null` or `undefined`');
 			}
 			var items = Object(arrayLike);
 			var mapping = arguments.length > 1;
@@ -40,7 +47,7 @@ if (!Array.from) {
 			if (arguments.length > 1) {
 				mapFn = arguments[1];
 				if (!isCallable(mapFn)) {
-					throw new TypeError('Array.from: when provided, the second argument must be a function');
+					throw new TypeError('When provided, the second argument to `Array.from` must be a function');
 				}
 				if (arguments.length > 2) {
 					T = arguments[2];
@@ -54,14 +61,14 @@ if (!Array.from) {
 			while (k < len) {
 				kValue = items[k];
 				if (mapFn) {
-					mappedValue = typeof T === 'undefined' ? mapFn(kValue, k) : mapFn.call(T, kValue, k);
+					mappedValue = typeof T == 'undefined' ? mapFn(kValue, k) : mapFn.call(T, kValue, k);
 				} else {
 					mappedValue = kValue;
 				}
 				defineProperty(A, k, {
-					value: mappedValue,
-					configurable: true,
-					enumerable: true
+					'value': mappedValue,
+					'configurable': true,
+					'enumerable': true
 				});
 				++k;
 			}
@@ -69,9 +76,9 @@ if (!Array.from) {
 			return A;
 		};
 		defineProperty(Array, 'from', {
-			value: from,
-			configurable: true,
-			writable: true
+			'value': from,
+			'configurable': true,
+			'writable': true
 		});
 	}());
 }
