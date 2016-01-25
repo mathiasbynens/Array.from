3fac6f656da7a61511679c56864c9cbf97bd4a58
Jake Champion <jakechampion.jake2@googlemail.com>
2016-01-25 15:38:12 +0000

    Conform to ES-Shim API interface

--- detect.js
+++
@@ -1,3 +0,0 @@
-if (!Array.from) {
-  require('./array-from');
-}
--- tests/tests.js
+++ tests/tests.js
@@ -1,184 +1,197 @@
 'use strict';
 
 var test = require('tape');
+var ES = require('es-abstract/es6');
 
-require('../array-from.js');
-
-test('Array.from has proper length', function (t) {
-	t.equal(Array.from.length, 1);
-	t.end();
-});
-
-test('Array.from is not enumerable', function (t) {
-	t.notOk(Array.propertyIsEnumerable('from'));
-	t.end();
-});
-
-test('requires an array-like object', function (t) {
-	t.throws(function () { Array.from(); }, TypeError);
-	t.throws(function () { Array.from(null); }, TypeError);
-	t.end();
-});
-
-test('throws with invalid lengths', function (t) {
-	t.throws(function () { Array.from({ length: Infinity }); }, RangeError);
-	t.throws(function () { Array.from({ length: Math.pow(2, 32) }); }, RangeError);
-	t.end();
-});
-
-test('swallows negative lengths', function (t) {
-	t.equal(Array.from({ length: -1 }).length, 0);
-	t.equal(Array.from({ length: -Infinity }).length, 0);
-	t.equal(Array.from({ length: -0 }).length, 0);
-	t.equal(Array.from({ length: - 42}).length, 0);
-	t.end();
-});
-
-test('works with primitives', function (t) {
-	t.deepEqual(Array.from(false), []);
-	t.deepEqual(Array.from(true), []);
-	t.deepEqual(Array.from(-Infinity), []);
-	t.deepEqual(Array.from(-0), []);
-	t.deepEqual(Array.from(0), []);
-	t.deepEqual(Array.from(1), []);
-	t.deepEqual(Array.from(Infinity), []);
-	t.end();
-});
-
-test('works with strings', function (t) {
-	t.deepEqual(Array.from(''), []);
-	t.deepEqual(Array.from('abc'), 'abc'.split(''));
-	t.end();
-});
-
-test('works with objects', function (t) {
-	t.deepEqual(Array.from({}), []);
-	t.deepEqual(Array.from({ a: 1 }), []);
-	t.end();
-});
-
-test('works with arrays', function (t) {
-	t.deepEqual(Array.from([]), []);
-	t.deepEqual(Array.from([1, 2, 3]), [1, 2, 3]);
-	t.deepEqual(Array.from([4, 5, 6]), [4, 5, 6]);
-	t.end();
-});
-
-test('it fills holes in arrays', function (t) {
-	var arr = [1, 2, 3];
-	delete arr[1];
-	t.deepEqual(Array.from(arr), [1, undefined, 3]);
-	t.deepEqual(Array.from([4, , 6]), [4, undefined, 6]);
-	t.end();
-});
-
-test('it includes Object.prototype values when it is polluted', function (t) {
-	Object.prototype[1] = 42;
-	t.deepEqual(Array.from({ length: 3, 0: 1, 2: 3 }), [1, 42, 3]);
-	delete Object.prototype[1];
-	t.end();
-});
-
-test('works with arraylike objects', function (t) {
-	t.deepEqual(Array.from({ length: 1 }), [void 0]);
-	t.deepEqual(Array.from({ 0: 'a', 1: 'b', length: 2 }), ['a', 'b']);
-	t.end();
-});
-
-test('throws with an invalid mapping function', function (t) {
-	t.throws(function () { Array.from([], undefined); }, TypeError);
-	t.throws(function () { Array.from([], null); }, TypeError);
-	t.throws(function () { Array.from([], false); }, TypeError);
-	t.throws(function () { Array.from([], true); }, TypeError);
-	t.throws(function () { Array.from([], {}); }, TypeError);
-	t.throws(function () { Array.from([], /a/g); }, TypeError);
-	t.throws(function () { Array.from([], 'foo'); }, TypeError);
-	t.throws(function () { Array.from([], 42); }, TypeError);
-	t.end();
-});
-
-test('works with a mapping function', function (t) {
-	var original = [1, 2, 3];
-	var actual = Array.from(original, function (value, index) {
-		t.equal(value, original[index], 'value and index are correct');
-		t.equal(arguments.length, 2, 'value and index are only arguments passed to the mapping function');
-		return value * 2;
-	});
-	t.deepEqual(actual, [2, 4, 6]);
-
-	t.test('accepts an object thisArg', function (st) {
-		var context = {};
-		Array.from(original, function (value, index) {
-			t.equal(this, context, 'given context is the actual context');
-		}, context);
-		st.end();
+function runTests(arrayFrom) {
+	test('from has proper length', function (t) {
+		t.equal(arrayFrom.length, 1);
+		t.end();
 	});
 
-	t.test('accepts a primitive thisArg', function (st) {
-		Array.from(original, function (value, index) {
-			st.equal(this.valueOf(), 42, 'context valueOf() is correct');
-			st.equal(Object.prototype.toString.call(this), '[object Number]', 'context "[[Class]]" is correct');
-		}, 42);
-		st.end();
+	var arrayFromMethodExists = ES.IsCallable(Array.from);
+
+	test('from is not enumerable', { skip: !arrayFromMethodExists }, function (t) {
+		t.notOk(Array.propertyIsEnumerable('from'));
+		t.end();
 	});
 
-	t.test('accepts a falsy thisArg', function (st) {
-		Array.from(original, function (value, index) {
-			st.equal(this.valueOf(), false, 'context valueOf() is correct');
-			st.equal(Object.prototype.toString.call(this), '[object Boolean]', 'context "[[Class]]" is correct');
-		}, false);
-		st.end();
+	test('requires an array-like object', function (t) {
+		t.throws(function () { arrayFrom(); }, TypeError);
+		t.throws(function () { arrayFrom(null); }, TypeError);
+		t.end();
 	});
 
-	t.end();
-});
-
-test('works when called from a non-constructor context', function (t) {
-	var from = Array.from;
-	t.deepEqual(Array.from.call(null, { length: 1, 0: 'a' }), ['a']);
-	t.deepEqual(from({ length: 1, 0: 'a' }), ['a']);
-	t.end();
-});
-
-test('no setters are called for indexes', { skip: !Object.defineProperty }, function (t) {
-	var MyType = function () {};
-	Object.defineProperty(MyType.prototype, '0', {
-		set: function (x) { throw new Error('setter called: ' + x); }
+	test('throws with invalid lengths', function (t) {
+		t.throws(function () { arrayFrom({ length: Infinity }); }, RangeError);
+		t.throws(function () { arrayFrom({ length: Math.pow(2, 32) }); }, RangeError);
+		t.end();
 	});
-	var myInstance = new MyType();
-	t.throws(function () { myInstance[0] = 'foo'; });
 
-	var actual = Array.from.call(MyType, { 0: 'abc', length: 1 });
-	t.deepEqual(actual, { 0: 'abc', length: 1 });
-	t.ok(actual instanceof MyType);
-	t.end();
-});
+	test('swallows negative lengths', function (t) {
+		t.equal(arrayFrom({ length: -1 }).length, 0);
+		t.equal(arrayFrom({ length: -Infinity }).length, 0);
+		t.equal(arrayFrom({ length: -0 }).length, 0);
+		t.equal(arrayFrom({ length: - 42}).length, 0);
+		t.end();
+	});
+
+	test('works with primitives', function (t) {
+		t.deepEqual(arrayFrom(false), []);
+		t.deepEqual(arrayFrom(true), []);
+		t.deepEqual(arrayFrom(-Infinity), []);
+		t.deepEqual(arrayFrom(-0), []);
+		t.deepEqual(arrayFrom(0), []);
+		t.deepEqual(arrayFrom(1), []);
+		t.deepEqual(arrayFrom(Infinity), []);
+		t.end();
+	});
+
+	test('works with strings', function (t) {
+		t.deepEqual(arrayFrom(''), []);
+		t.deepEqual(arrayFrom('abc'), 'abc'.split(''));
+		t.end();
+	});
+
+	test('works with objects', function (t) {
+		t.deepEqual(arrayFrom({}), []);
+		t.deepEqual(arrayFrom({ a: 1 }), []);
+		t.end();
+	});
+
+	test('works with arrays', function (t) {
+		t.deepEqual(arrayFrom([]), []);
+		t.deepEqual(arrayFrom([1, 2, 3]), [1, 2, 3]);
+		t.deepEqual(arrayFrom([4, 5, 6]), [4, 5, 6]);
+		t.end();
+	});
+
+	test('it fills holes in arrays', function (t) {
+		var arr = [1, 2, 3];
+		delete arr[1];
+		t.deepEqual(arrayFrom(arr), [1, undefined, 3]);
+		t.deepEqual(arrayFrom([4, , 6]), [4, undefined, 6]);
+		t.end();
+	});
+
+	test('it includes Object.prototype values when it is polluted', function (t) {
+		Object.prototype[1] = 42;
+		t.deepEqual(arrayFrom({ length: 3, 0: 1, 2: 3 }), [1, 42, 3]);
+		delete Object.prototype[1];
+		t.end();
+	});
+
+	test('works with arraylike objects', function (t) {
+		t.deepEqual(arrayFrom({ length: 1 }), [void 0]);
+		t.deepEqual(arrayFrom({ 0: 'a', 1: 'b', length: 2 }), ['a', 'b']);
+		t.end();
+	});
+
+	test('throws with an invalid mapping function', function (t) {
+		t.throws(function () { arrayFrom([], undefined); }, TypeError);
+		t.throws(function () { arrayFrom([], null); }, TypeError);
+		t.throws(function () { arrayFrom([], false); }, TypeError);
+		t.throws(function () { arrayFrom([], true); }, TypeError);
+		t.throws(function () { arrayFrom([], {}); }, TypeError);
+		t.throws(function () { arrayFrom([], /a/g); }, TypeError);
+		t.throws(function () { arrayFrom([], 'foo'); }, TypeError);
+		t.throws(function () { arrayFrom([], 42); }, TypeError);
+		t.end();
+	});
+
+	test('works with a mapping function', function (t) {
+		var original = [1, 2, 3];
+		var actual = arrayFrom(original, function (value, index) {
+			t.equal(value, original[index], 'value and index are correct');
+			t.equal(arguments.length, 2, 'value and index are only arguments passed to the mapping function');
+			return value * 2;
+		});
+		t.deepEqual(actual, [2, 4, 6]);
+
+		t.test('accepts an object thisArg', function (st) {
+			var context = {};
+			arrayFrom(original, function (value, index) {
+				t.equal(this, context, 'given context is the actual context');
+			}, context);
+			st.end();
+		});
+
+		t.test('accepts a primitive thisArg', function (st) {
+			arrayFrom(original, function (value, index) {
+				st.equal(this.valueOf(), 42, 'context valueOf() is correct');
+				st.equal(Object.prototype.toString.call(this), '[object Number]', 'context "[[Class]]" is correct');
+			}, 42);
+			st.end();
+		});
+
+		t.test('accepts a falsy thisArg', function (st) {
+			arrayFrom(original, function (value, index) {
+				st.equal(this.valueOf(), false, 'context valueOf() is correct');
+				st.equal(Object.prototype.toString.call(this), '[object Boolean]', 'context "[[Class]]" is correct');
+			}, false);
+			st.end();
+		});
+
+		t.end();
+	});
+
+	test('works when called from a non-constructor context', function (t) {
+		var from = arrayFrom;
+		t.deepEqual(from.call(null, { length: 1, 0: 'a' }), ['a']);
+		t.deepEqual(arrayFrom({ length: 1, 0: 'a' }), ['a']);
+		t.end();
+	});
+
+	test('no setters are called for indexes', { skip: !Object.defineProperty }, function (t) {
+		var MyType = function () {};
+		Object.defineProperty(MyType.prototype, '0', {
+			set: function (x) { throw new Error('setter called: ' + x); }
+		});
+		var myInstance = new MyType();
+		t.throws(function () { myInstance[0] = 'foo'; });
+
+		var actual = arrayFrom.call(MyType, { 0: 'abc', length: 1 });
+		t.deepEqual(actual, { 0: 'abc', length: 1 });
+		t.ok(actual instanceof MyType);
+		t.end();
+	});
 
 
-test('allows shift without throwing type error', function (t) {
-	t.doesNotThrow(Array.prototype.shift.bind(Array.from([1,2,3])));
-	t.end();
-});
+	test('allows shift without throwing type error', function (t) {
+		t.doesNotThrow(Array.prototype.shift.bind(arrayFrom([1,2,3])));
+		t.end();
+	});
 
-// These tests take way too long to execute, sadly:
-/*
-test.skip('works with very large lengths', function (t) {
-	t.equals(Array.from({ length: 0xFFFFFFFF }).length, 0xFFFFFFFF);
-	var Constructor = function (length) {
-		this.length = length;
-		return this;
-	};
-	t.equals(Array.from.call(Constructor, { length: 0xFFFFFFFF }).length, 0xFFFFFFFF);
-	t.equals(Array.from.call(Constructor, { length: 0xFFFFFFFF + 1 }).length, 0xFFFFFFFF + 1 );
-	t.equals(Array.from.call(Constructor, { length: 0x1FFFFFFFFFFFFF }).length, 0x1FFFFFFFFFFFFF);
-	t.equals(Array.from.call(Constructor, { length: 0x1FFFFFFFFFFFFF + 1 }).length, 0x1FFFFFFFFFFFFF);
-	t.end();
-});
-*/
+	// These tests take way too long to execute, sadly:
+	/*
+	test('works with very large lengths', function (t) {
+		t.equals(arrayFrom({ length: 0xFFFFFFFF }).length, 0xFFFFFFFF);
+		var Constructor = function (length) {
+			this.length = length;
+			return this;
+		};
+		t.equals(from.call(Constructor, { length: 0xFFFFFFFF }).length, 0xFFFFFFFF);
+		t.equals(from.call(Constructor, { length: 0xFFFFFFFF + 1 }).length, 0xFFFFFFFF + 1 );
+		t.equals(from.call(Constructor, { length: 0x1FFFFFFFFFFFFF }).length, 0x1FFFFFFFFFFFFF);
+		t.equals(from.call(Constructor, { length: 0x1FFFFFFFFFFFFF + 1 }).length, 0x1FFFFFFFFFFFFF);
+		t.end();
+	});
+	*/
 
-var functionsHaveNames = function f() {}.name === 'f';
+	var functionsHaveNames = function f() {}.name === 'f';
 
-test('returns the correct name when called with toString', { skip: !functionsHaveNames }, function (t) {
-	t.equal(Array.from.name, 'from', 'Array#from has name "from"');
-	t.end();
-});
+	test('returns the correct name when called with toString', { skip: !functionsHaveNames }, function (t) {
+		t.equal(arrayFrom.name, 'from', 'Array#from has name "from"');
+		t.end();
+	});
+}
+
+var from = require('../index');
+
+runTests(from.implementation);
+
+runTests(from.getPolyfill());
+
+from.shim();
+
+runTests(Array.from);
